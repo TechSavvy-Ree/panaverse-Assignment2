2de61eb308fbed5a6f5859d1591845637b689cf1
TechSavvy-Ree <ranya7476@gmail.com>
2023-12-01 09:29:50 -0800

    calculates the volume of a cube

---
+++ Ex-2.ts
@@ -0,0 +1,6 @@
+// Exercise 2: Write a program that calculates the volume of a cube.
+// In a cube, all the sides have same length therefore the volume of cube is (V= L^3) L*L*L.
+function volumeOfCube(length:number) {
+    console.log(`The value of length=${length} \nVolume of cube=`,length*length*length);
+}
+volumeOfCube(5);
---
+++ Ex-3.ts
@@ -0,0 +1,16 @@
+// Exercise 3:Write a program that checks if a given number is positive, negative, or zero.
+// create an Array which store different numbers, then we check it by applying conditions. 
+let tempNumber=[-90,0,76];
+for (let number of tempNumber)
+{
+ console.log(number);
+ if (number>0) {
+    console.log("Given number is positive");
+    }
+ else if(number<0){
+        console.log("Given number is nagative");
+    }
+ else{console.log("Given number is zero");
+    }
+    
+}
