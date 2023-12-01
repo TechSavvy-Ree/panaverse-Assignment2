0452b52661442bed90ec64cca8c1234d8af954c4
TechSavvy-Ree <ranya7476@gmail.com>
2023-12-01 08:45:02 -0800

    calculating the area of rectangle

---
+++ Ex-1.ts
@@ -0,0 +1,17 @@
+// Exercise 1:  Write a program that calculates the area of a rectangle.
+// Declare a function as *areaOfRectangle & pass the parameters (Length, Width) with type.
+
+console.log(".........Area of Rectangule?.........\n");
+
+function AreaOfRectangular(length:number,width:number) {
+    console.log(`The value of length=${length} \nAnd width=${width}\nArea of Rectangular=`,length*width);
+    }
+    AreaOfRectangular(20,30);
+    // ..................Another Way of calculating area of rectangle..................
+   /* function areaOfRectangle(length:number, width:number): number {
+        // Calculate area
+        const area = length * width;
+      
+        // Return the calculated area
+        return area;
+    }*/
