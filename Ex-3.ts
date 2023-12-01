1dcb0c29e429a119a5ec1cece6353454c5e0e3a1
TechSavvy-Ree <ranya7476@gmail.com>
2023-12-01 09:34:53 -0800

    Check number type wether positive, negtive or zero

--- Ex-3.ts
+++ Ex-3.ts
@@ -1,6 +1,6 @@
 // Exercise 3:Write a program that checks if a given number is positive, negative, or zero.
 // create an Array which store different numbers, then we check it by applying conditions. 
-let tempNumber=[-90,0,76];
+let tempNumber=[-90,-150,0,76,298];
 for (let number of tempNumber)
 {
  console.log(number);
